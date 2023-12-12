fa7328c06b9991e578bb021ef3ea00bd3c3f166d
EnderHDMC <16001028+EnderHDMC@users.noreply.github.com>
2023-12-12 13:08:10 +0200

    Remove dead scene

--- src/lib/scene.ts
+++ src/lib/scene.ts
@@ -2,22 +2,8 @@ import kaboom, { type Comp, type Key, type RotateComp } from 'kaboom';
 import 'kaboom/global';
 
 export const createGame = (canvas: HTMLCanvasElement) => {
-	// initialize context
 	kaboom({ canvas });
-
 	scene('game', gameScene);
-
-	scene('lose', (score) => {
-		add([sprite('bean'), pos(width() / 2, height() / 2 - 80), scale(2), anchor('center')]);
-
-		// display score
-		add([text(score), pos(width() / 2, height() / 2 + 80), scale(2), anchor('center')]);
-
-		// go back to game with space is pressed
-		onKeyPress('space', () => go('game'));
-		onClick(() => go('game'));
-	});
-
 	go('game');
 };
 
