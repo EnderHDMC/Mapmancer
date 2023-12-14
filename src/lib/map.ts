425db711a34e7a964ded4b030fab3917446f08c5
EnderHDMC <16001028+EnderHDMC@users.noreply.github.com>
2023-12-14 13:12:46 +0200

    A bit better dungeon generation

--- src/lib/map.ts
+++ src/lib/map.ts
@@ -47,6 +47,24 @@ const mapTiles: { [key: string]: tileGen } = {
 			tile({ isObstacle: true })
 		]
 	},
+	f: {
+		floor: '0',
+		generator: () => [
+			sprite('wall_edge_bottom_left'),
+			area({ shape: new Rect(vec2(0), 4, 16) }),
+			body({ isStatic: true }),
+			tile({ isObstacle: true })
+		]
+	},
+	g: {
+		floor: '0',
+		generator: () => [
+			sprite('wall_edge_bottom_right'),
+			area({ shape: new Rect(vec2(12, 0), 4, 16) }),
+			body({ isStatic: true }),
+			tile({ isObstacle: true })
+		]
+	},
 	c: {
 		floor: '0',
 		generator: () => [
@@ -130,7 +148,7 @@ const mapTiles: { [key: string]: tileGen } = {
 	},
 	n: {
 		floor: '0',
-		generator: () => [sprite('floor_stairs'), body({ isStatic: true }), area(), tile()]
+		generator: () => [sprite('floor_stairs'), area(), tile({}), 'stairs']
 	},
 	' ': {
 		floor: '1',
@@ -145,19 +163,36 @@ function roomToFloor(room: string[]) {
 	return floor;
 }
 
-function generateRoom(x: number, y: number) {
-	const qwe = x === 1 && y === 1 ? 'n' : ' ';
+function generateRoom(
+	north: boolean,
+	south: boolean,
+	east: boolean,
+	west: boolean,
+	stairs: boolean
+) {
+	const end = stairs ? 'n' : ' ';
+	const wes = west ? 'l' : ' ';
+	const eas = east ? 'r' : ' ';
+	const so0 = south ? 'w' : ' ';
+	const so1 = south ? 't' : ' ';
+	const so2 = south ? 'w' : 'd';
+	const so3 = south ? 'w' : 'c';
+	const no0 = north ? 'y' : ' ';
+	const no1 = north ? 'w' : ' ';
+	const no2 = north ? 'y' : 'g';
+	const no3 = north ? 'y' : 'f';
+	
 	const tiles: string[][] = [
-		['q', 'y', 'y', 'y', 'y', 'y', 'y', 'y', 'y', 's'],
-		['c', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'd'],
+		['q', 'y', 'y', no2, no0, no0, no3, 'y', 'y', 's'],
+		['c', 'w', 'w', 'w', no1, no1, 'w', 'w', 'w', 'd'],
 		['l', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'r'],
 		['l', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'r'],
-		['l', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'r'],
-		['l', ' ', ' ', ' ', ' ', ' ', ' ', '$', ' ', 'r'],
-		['l', ' ', ' ', ' ', ' ', ' ', qwe, ' ', ' ', 'r'],
+		[wes, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', eas],
+		[wes, ' ', ' ', ' ', ' ', ' ', ' ', '$', ' ', eas],
+		[wes, ' ', ' ', ' ', ' ', ' ', end, ' ', ' ', eas],
 		['l', ' ', '$', ' ', ' ', ' ', ' ', 'e', ' ', 'r'],
-		['a', 't', 't', 't', 't', 't', 't', 't', 't', 'b'],
-		['w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w']
+		['a', 't', 't', 't', so1, so1, 't', 't', 't', 'b'],
+		['w', 'w', 'w', so2, so0, so0, so3, 'w', 'w', 'w']
 	];
 	return tiles.map((r) => r.join(''));
 }
@@ -169,7 +204,18 @@ function transpose(arr: any[][]) {
 function joinRooms(rooms: string[][][]) {
 	const rows = rooms.map((room) => {
 		const transposed = transpose(room);
-		return transposed.map((column) => column.join('--'));
+		return transposed.map((column, i) => {
+			const joiners: { [k: number]: string } = {
+				3: 'yy',
+				4: 'ww',
+				5: '  ',
+				6: 'tt',
+				7: 'ww'
+			};
+
+			let joiner = joiners[i] || '--';
+			return column.join(joiner);
+		});
 	});
 
 	const map = ([] as string[]).concat(...rows);
@@ -179,9 +225,13 @@ function joinRooms(rooms: string[][][]) {
 function generateMap() {
 	randSeed(0);
 
+	const room1 = generateRoom(true, false, false, true, false);
+	const room2 = generateRoom(true, false, true, false, false);
+	const room3 = generateRoom(false, true, false, true, false);
+	const room4 = generateRoom(false, true, true, false, true);
 	const rooms = [
-		[generateRoom(0, 0), generateRoom(1, 0)],
-		[generateRoom(1, 0), generateRoom(1, 1)]
+		[room1, room2],
+		[room3, room4]
 	];
 	const room = joinRooms(rooms);
 	const floorMap = roomToFloor(room);
@@ -210,7 +260,10 @@ function generateMap() {
 			r: mapTiles['r'].generator,
 			q: mapTiles['q'].generator,
 			s: mapTiles['s'].generator,
-			y: mapTiles['y'].generator
+			y: mapTiles['y'].generator,
+			n: mapTiles['n'].generator,
+			f: mapTiles['f'].generator,
+			g: mapTiles['g'].generator
 		}
 	});
 
